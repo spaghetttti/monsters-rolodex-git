dacd5cc6a7fc4bc37756b1e0634c3362c016ca85
AsilM <asil9802mum@gmail.com>
2021-03-21 17:13:09 +0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { CardList } from './components/card-list/card-list.component.jsx'
+import { Card } from "./components/card/card.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component" ;
+                      
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component  {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters : [],
+      searchField : ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response => Response.json())
+    .then(users => this.setState({monsters : users}))
+  }
+  
+  handleChange = (e) => {
+    this.setState({ searchField : e.target.value}, () => console.log(this.state))  
+  } 
+
+  render() {
+
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    ) 
+
+    return ( 
+      <div className="App">
+          
+        <SearchBox 
+          placeholder = ' search monsters'
+          handleChange = { this.handleChange}
+        />
+        <h1 className = "headline">
+            Monsters Rolodex
+        </h1>
+        <CardList monsters = {filteredMonsters} > 
+              
+        </CardList>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {
+              this.state.monsters.map(monsters => ( 
+             <p> <i> {monsters.name} </i> </p>))
+            }
+          </p>
+       </header>
+          
+        </div>
+     )
+
+
+  }
 }
 
 export default App;
